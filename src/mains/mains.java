060a6ce5128dec160cba672bf192cd34ba710e3f
Fidioc <chadoin@et.esiea.fr>
2020-12-11 11:29:55 +0100

    modifie main to include enum variable

--- src/mains/mains.java
+++ src/mains/mains.java
@@ -1,10 +1,17 @@
 package mains;
+import chanson.Genre;
+import livreaudio.Categuories;
+import livreaudio.Langue;
 import livreaudio.livresAudios;
 public class mains {
 	public static void main(String[] args) {
 		livresAudios livre;
+		Categuories [] cate = Categuories.values();
+		Langue [] langue = Langue.values();
+		Genre [] genre = Genre.values();
 		livre = new livresAudios("aventure","français","pirate des caraibes","moi","long",300,2);
-		System.out.println(livre.getTitre());		
+		System.out.println(livre.getTitre());
+				
 		
 	}
 
