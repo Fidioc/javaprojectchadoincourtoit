47f733bdf1f92df9d8c2091da7f267dd4d94757a
Fidioc <chadoin@et.esiea.fr>
2020-12-27 16:19:05 +0100

    correction

--- src/mains/mains.java
+++ src/mains/mains.java
@@ -1,21 +1,31 @@
 package mains;
+
 import java.util.Scanner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import chanson.Chansons;
 import chanson.Genre;
 import livreaudio.Categuories;
 import livreaudio.Langue;
 import livreaudio.livresAudios;
+
 public class mains {
+	static Categuories [] cate = Categuories.values();
+	static Langue [] langue = Langue.values();
+	static Genre [] genre = Genre.values();
+	static int ID = 0;
+
+	
 	public static void main(String[] args) {
-		livresAudios livre;
-		Categuories [] cate = Categuories.values();
-		Langue [] langue = Langue.values();
-		Genre [] genre = Genre.values();
-		livre = new livresAudios(cate[1],langue[1],"pirate des caraibes","moi","long",300,2);
+
+		livresAudios livre = new livresAudios(cate[1],langue[1],"pirate des caraibes","moi","long",300,2);
 		System.out.println(livre.getTitre());
 
 		while (true) {
 		System.out.println("appuyer sur s pour sauvegarder: ");
-		Scanner scan = nex Scanner(System.in);
+		Scanner scan = next Scanner(System.in);
 		String choice = scan.next();
 		switch (choice){
 			case "s" :System.out.println("vous avez pressé le bouton" + choice);break;
@@ -23,6 +33,7 @@ public class mains {
 			default : System.out.println("Vous avez pressé le bouton" + choice);break;
 		}
 		}
+
 	}
 
 }
