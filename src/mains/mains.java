49ae7e793e159d018d38b674de554eb9b5671ef2
Fidioc <chadoin@et.esiea.fr>
2020-12-27 16:31:51 +0100

    ajout de map

--- src/mains/mains.java
+++ src/mains/mains.java
@@ -2,6 +2,9 @@ package mains;
 
 import java.util.Scanner;
 
+import Album.Albums;
+import Playlist.Playlist;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,16 +22,23 @@ public class mains {
 
 	
 	public static void main(String[] args) {
+		Map<Integer,Chansons> chansons=new HashMap<>();
+		Map<Integer,Albums> album=new HashMap<>();
+		Map<Integer,livresAudios> livreaudio=new HashMap<>();
+		Map<Integer,Playlist> playlist=new HashMap<>();
+		
+		int i=0;
 
-		livresAudios livre = new livresAudios(cate[1],langue[1],"pirate des caraibes","moi","long",300,2);
-		System.out.println(livre.getTitre());
 
 		while (true) {
 		System.out.println("appuyer sur s pour sauvegarder: ");
-		Scanner scan = next Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		String choice = scan.next();
 		switch (choice){
-			case "s" :System.out.println("vous avez pressé le bouton" + choice);break;
+			case "s" :
+				System.out.println("vous avez pressé le bouton" + choice);
+				chansons.put(ID++, new Chansons(genre[i], choice, choice, choice, 300, ID));
+				break;
 			case "q" :System.out.println("Vous avez pressé le bouton" + choice);break;
 			default : System.out.println("Vous avez pressé le bouton" + choice);break;
 		}
