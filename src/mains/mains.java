7bb5b610a1c8d52bb5f83c3503a69dca2b8efdba
melcici <fastionpunch@gmail.com>
2020-12-27 15:51:38 +0100

    update main

--- src/mains/mains.java
+++ src/mains/mains.java
@@ -1,4 +1,5 @@
 package mains;
+import java.util.Scanner;
 import chanson.Genre;
 import livreaudio.Categuories;
 import livreaudio.Langue;
@@ -11,8 +12,17 @@ public class mains {
 		Genre [] genre = Genre.values();
 		livre = new livresAudios(cate[1],langue[1],"pirate des caraibes","moi","long",300,2);
 		System.out.println(livre.getTitre());
-				
-		
+
+		while (true) {
+		System.out.println("appuyer sur s pour sauvegarder: ");
+		Scanner scan = nex Scanner(System.in);
+		String choice = scan.next();
+		switch (choice){
+			case "s" :System.out.println("vous avez pressé le bouton" + choice);break;
+			case "q" :System.out.println("Vous avez pressé le bouton" + choice);break;
+			default : System.out.println("Vous avez pressé le bouton" + choice);break;
+		}
+		}
 	}
 
 }
